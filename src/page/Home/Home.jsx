44a8c6504b37812660e41ba81a16af3f5ae758c4
Facundo Brunetti <facundobrunetti99@gmail.com>
2025-01-14 12:21:08 -0300

    first commit

---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../../component/navbar/Navbar'
+import "../Home/Home.css"
+const Home = () => {
+  return (
+    <>
+    <div className='main'>
+
+    <Navbar></Navbar>
+    </div>
+    
+    <div >Home</div>
+    </>
+    
+  )
+}
+
+export default Home
