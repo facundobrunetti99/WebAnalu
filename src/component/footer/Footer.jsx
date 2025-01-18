a179d5dc86f6464d63a4465bdf6fd03a48ff0c08
Facundo Brunetti <facundobrunetti99@gmail.com>
2025-01-17 23:20:18 -0300

    added fucntion footer div

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "../footer/Footer.css"
-const Footer = () => {
-  return (
-    <footer className='footer-container'>
 
+
+
+const Footer = () => {
+
+  const functionDiv = () => {
+    const elements = [];
+    for (let i = 0; i < 3; i++) {
+      elements.push(
+        <div key={i} className="col">
+          {i}
+        </div>
+      );
+    }
+    return elements;
+  };
+  return (
+    <footer className='footer-container coteiner'>
+        <div className="conteiner conteiner-items-footer row">
+         {functionDiv()}
+        </div>
     </footer>
   )
 }
