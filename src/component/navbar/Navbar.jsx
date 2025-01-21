b0c138147a184cf0cba17c10e5fa4820a6b0e424
Facundo Brunetti <facundobrunetti99@gmail.com>
2025-01-21 02:11:44 -0300

    changes in img-cake-Link

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -20,12 +20,15 @@ const Navbar = () => {
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <div className="div-logo-analu">
+              
                 <Link href="" className="img-cake" to="/">
                   <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAcxJREFUWEftV0tygkAQ7UcuQnYJlQOkJIt4ieAuchL1JNGd5hKyiFYOkDLZhYPIdKoJg3wG+YjlRlZTzND9uvu97gF04QcX9k8nAXh6GD1LAPs9hZ8/y7BLMJ0BPN6N7BuLf8UpE0+3u/fZFUCvGdD1rTKqFNkgfkv257CwOAagiiclDgyclwkI0y7R1H7DFEQMP0vYHICzOj+gCyOFoQaRAsiyWqeViTtJy5QJEMZEZP+rBv52t5zL2gjgFFlVlaEQ4HyzW/k5AEI6VrwuIqyta4sDruNxfJwp2HyvhlcAvWRAatt0DjQuARGFTJgpRYHJ+MAZjUE80azWqokUZqbzMb8inhAoHl61HEh5ZWgc7r23Tg2VCZjTuGwbe0sdCYt2RZYgiIYlgljLOmpisgnxu8N7poDBARjPRrB1ABKHaeMwKC2EBf/jaxnoPdfxZC7IN8YHFoZa5rUl0J0qMVqKuqrWSbqLwONBJWAbkzDbKiUcIZFMM1k3YXzV+c4AWjS7o0evABpnQFjaV9pzdpJGlJ22uQuJ63hyy83q/Cw4IoXb0oVEs50VvxoaTj9A4gaFhb6M5IZRPx7aW+n8Y9LeVUWH7MtQVzvXDPwBe019MD2qc5QAAAAASUVORK5CYII=" />
                 </Link>
+                <div className="div-text-cake">
                 <Link className="p-analu" to="/">
                   Analu <p className="p-pasteleria">Pasteleria</p>
                 </Link>
+                </div>
               </div>
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
